22ca8586421f6ed12531af0d3741ba02148e216a
leejisuu <dlwltn966@gmail.com>
2026-02-02 17:54:42 +0900

    4-6. 숨겨져 있는 도메인 개념 도출하기

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,5 +1,6 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.*;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
@@ -9,11 +10,15 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Advanced();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
+        GameConfig gameConfig = new GameConfig(
+                new Advanced(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler()
+        );
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        // 생성자의 변경이 없도록 게임 설정 정보룰 객체로 한번 포장
+        // 과도한 포장은 오버엔지니어링이 될 수 있으므로 조심
+        Minesweeper minesweeper = new Minesweeper(gameConfig);
         minesweeper.initialize();
         minesweeper.run();
     }
